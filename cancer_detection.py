599ef4034b73bb84664b480a3714ea298c8a00b6
MOHAMED  ASARUDHEEN <masarudheena@gmail.com>
2018-08-07 07:48:48 +0530

    Add files via upload

--- cancer_detection.py
+++ cancer_detection.py
@@ -5,3 +5,11 @@ Created on Sun Aug  5 19:19:25 2018
 @author: ma
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import io
+import time
+import json
+
+data = pd.read_csv("data.csv")
